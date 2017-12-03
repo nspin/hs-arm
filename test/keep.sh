055eaabed83483ea0ab9be654ac6086543b8c8d2
Nick Spinale <spinalen@carleton.edu>
2017-12-02 20:56:47 -0500

    Add generated MRAS to keep script

--- test/keep.sh
+++ test/keep.sh
@@ -11,13 +11,14 @@ root="$here/../generated-sources"
 rm -r "$root"
 mkdir -p "$root"
 
+go_rw arm-mras.dtd-src.sysreg
+go_rw arm-mras.dtd-src.a64
+go_rw arm-mras.dtd-src.aarch32
+go_rw arm-mras.values-src
+
 go_rw arm-binutils-tables.c
 go_rw arm-binutils-tables.types-src
 go_rw arm-binutils-tables.values-src
 
 go_rw arm-go-tables.types-src
 go_rw arm-go-tables.values-src
-
-go_rw arm-mras.dtd-src.sysreg
-go_rw arm-mras.dtd-src.a64
-go_rw arm-mras.dtd-src.aarch32
