8c977b3ee3f3197c57f720d6d67bd56893a714dc
Nick Spinale <spinalen@carleton.edu>
2017-12-25 02:16:43 -0500

    Clean up example

--- examples/dump.hs
+++ examples/dump.hs
@@ -6,8 +6,8 @@ import Control.Monad
 
 main :: IO ()
 main = do
-    (start, t) <- elfText "../test/nix-results/test.busybox/busybox"
-    forM_ (zip [start, start + 4..] t) $ \(offset, w) -> do
+    (start, words) <- elfText "../test/nix-results/test.busybox/busybox"
+    forM_ (zip [start, start + 4..] words) $ \(offset, w) -> do
         putStrLn $ hex offset ++ "   " ++ hex w ++ "   " ++
             case decode w of
                 Nothing -> ".inst   " ++ hex w
